0b038bb81d6d98f082d7793b1e7e75a7126d436b
trihoangdev <louishoang.it@gmail.com>
2025-09-03 07:22:11 -0500

    Render task ra giao diện

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('Hello!');
+let tasks = [
+  {
+    title: "Rua chen",
+    completed: true,
+  },
+  {
+    title: "Quet nha",
+    completed: false,
+  },
+  {
+    title: "Lau nha",
+    completed: true,
+  },
+];
+
+const taskList = document.querySelector("#task-list");
+
+let html = tasks
+  .map(
+    (task) => `
+  <li class="task-item ${task.completed ? "uncompleted" : "completed"}">
+      <span class="task-title">${task.title}</span>
+      <div class="task-action">
+          <button class="task-btn edit">Edit</button>
+          <button class="task-btn done">${
+            task.completed ? "Mark as undone" : "Mark as done"
+          }</button>
+          <button class="task-btn delete">Delete</button>
+      </div>
+  </li>
+  </ul>
+  `
+  )
+  .join("");
+
+taskList.innerHTML = html;
