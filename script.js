9ebac9ce1808027d8a88e167ad2b7f47686d6d08
trihoangdev <louishoang.it@gmail.com>
2025-09-03 20:35:00 -0500

    Thêm công việc mới

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 let tasks = [
-  {
-    title: "Rua chen",
-    completed: true,
-  },
-  {
-    title: "Quet nha",
-    completed: false,
-  },
-  {
-    title: "Lau nha",
-    completed: true,
-  },
+  // {
+  //   title: "Rua chen",
+  //   completed: true,
+  // },
+  // {
+  //   title: "Quet nha",
+  //   completed: false,
+  // },
+  // {
+  //   title: "Lau nha",
+  //   completed: true,
+  // },
 ];
 
+// Get elements
 const taskList = document.querySelector("#task-list");
+const todoInput = document.querySelector("#todo-input");
+const todoForm = document.querySelector("#todo-form");
 
-let html = tasks
-  .map(
-    (task) => `
-  <li class="task-item ${task.completed ? "uncompleted" : "completed"}">
+let render = (tasks) => {
+  let html = tasks
+    .map(
+      (task) => `
+  <li class="task-item ${task.completed ? "completed" : "uncompleted"}">
       <span class="task-title">${task.title}</span>
       <div class="task-action">
           <button class="task-btn edit">Edit</button>
@@ -30,7 +34,20 @@ let html = tasks
   </li>
   </ul>
   `
-  )
-  .join("");
+    )
+    .join("");
 
-taskList.innerHTML = html;
+  taskList.innerHTML = html;
+};
+
+todoForm.onsubmit = function (e) {
+  e.preventDefault();
+  let value = todoInput.value.trim();
+  if (!value) {
+    alert("Please do something...");
+    return;
+  }
+  tasks.push({ title: value, completed: false });
+  todoInput.value = "";
+  render(tasks);
+};
